b58ea7c97040a7a746930197676b5f23e9a5f3b2
AnishRao486 <80604309+AnishRao486@users.noreply.github.com>
2021-05-18 21:26:47 -0400

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,16 @@
+function send() {
+    console.log("farty doodle")
+    number1 = document.getElementById("input_number1").value;
+    number2 = document.getElementById("input_number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    player_question = "<h4>" + number1 + "X" + number2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row= player_question + input_box + check_button;
+
+    document.getElementById("output").innerHTML = row;
+
+    document.getElementById("input_number1").value = "";
+    document.getElementById("input_number2").value = "";
+}
